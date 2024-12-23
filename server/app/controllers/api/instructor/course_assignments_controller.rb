087654c97a116067c7f3d3402a8b8ef7f7fae052
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-12-23 11:10:25 +0700

    fix5

--- server/app/controllers/api/instructor/course_assignments_controller.rb
+++ server/app/controllers/api/instructor/course_assignments_controller.rb
@@ -1,17 +1,14 @@
 module Api::Instructor
   class CourseAssignmentsController < ApplicationController
-    before_action :set_course
     before_action :set_course_assignment, only: :update_status
 
     def index
-      @q = CourseAssignment.for_teacher(current_teacher.id)
-                           .includes(:course, :user)
-                           .ransack(params[:q])
-      @pagy, @course_assignments = pagy @q.result
+      course_assignments = CourseAssignmentService.fetch_course_assignments(current_teacher, params)
+      @pagy, @course_assignments = pagy course_assignments
 
       json_response(
         message: {
-          course_assignments: @course_assignments,
+          course_assignments: formatted_course_assignments(@course_assignments),
           pagy: pagy_res(@pagy)
         },
         status: :ok
@@ -19,11 +16,7 @@ module Api::Instructor
     end
 
     def update_status
-      result = CourseAssignmentService.update_status(
-        teacher: current_teacher,
-        course_id: params[:course_id],
-        course_assignment: @course_assignment,
-        status: params[:status]
+      result = CourseAssignmentService.update_status(@course_assignment, params[:status]
       )
 
       if result[:success]
@@ -38,13 +31,25 @@ module Api::Instructor
     end
 
     private
-
-    def set_course
-      @course = Course.find_by(id: params[:course_id])
-    end
-
     def set_course_assignment
       @course_assignment = CourseAssignment.find_by(id: params[:id])
+    end
+    
+    def formatted_course_assignments course_assignments
+      course_assignments.map do |course_assignment|
+        {
+          id: course_assignment.id,
+          full_name: course_assignment.full_name,
+          course_id: course_assignment.course_id,
+          assigned_at: course_assignment.assigned_at,
+          status: course_assignment.status,
+          created_at: course_assignment.created_at,
+          updated_at: course_assignment.updated_at,
+          course_title: course_assignment.title,
+          course_level: course_assignment.level,
+          course_image_url: course_assignment.image_url
+        }
+      end
     end
   end
 end
